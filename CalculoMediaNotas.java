918dbb50553d23570fc7f051eef1803bd85edd8b
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-25 16:54:46 -0300

    Add files via upload

---
+++ CalculoMediaNotas.java
@@ -0,0 +1,16 @@
+public class CalculoMediaNotas {
+
+    public static void main(String[] args) {
+
+        double nota1 = 5.0;
+        double nota2 = 6.0;
+        double nota3 = 6.5;
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Nota 3: " + nota3);
+        System.out.println("Média: " + media);
+    }
+}
