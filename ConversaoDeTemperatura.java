f05a8b2e6ff71daab643d920243a19f4ca6eeedf
AgathaKarenne <142948129+AgathaKarenne@users.noreply.github.com>
2023-09-25 17:42:02 -0300

    Add files via upload

---
+++ ConversaoDeTemperatura.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ConversaoDeTemperatura {
+
+    /*
+     * Escreva um programa Java que declare uma variável do tipo double para
+     * representar uma temperatura em graus Celsius.
+     * Em seguida, converta essa temperatura para Fahrenheit usando a
+     * fórmula:Fahrenheit = (Celsius * 9/5) + 32 e exiba o resultado.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite a temperatura em graus Celsius :");
+        double ConversaoDeTemperatura = scanner.nextDouble();
+
+        scanner.close();
+
+        double temperaturafahrenheit = (Celsius * 9 / 5) + 32;
+
+        System.out.println("a temperatura em fahrenheit é: + temperaturafahrenheit");
+
+    }
+
+}
